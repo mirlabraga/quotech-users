4623bfaa79ccba09421976e1cbf7dbbf67c68817
Mirla Chucre <mirlabraga@gmail.com>
2021-04-26 12:26:18 +0100

    Add Cors

--- src/main/java/com/quotech/quotechusers/QuotechUsersApplication.java
+++ src/main/java/com/quotech/quotechusers/QuotechUsersApplication.java
@@ -2,6 +2,9 @@ package com.quotech.quotechusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class QuotechUsersApplication {
@@ -10,4 +13,13 @@ public class QuotechUsersApplication {
 		SpringApplication.run(QuotechUsersApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/users").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
